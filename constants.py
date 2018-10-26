4dd0a409925392443213cc5715a8e6555bad6d9e
SamGRosen <7624861+SamGRosen@users.noreply.github.com>
2018-10-26 17:48:44 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,11 +2,11 @@ import numpy as np
 np.random.seed(1)
 
 d = 100 # size of square in km
-max_start_pop = 10000
-min_start_pop = 500
 n = 2 # number of clans
 s_0 = 17.0/18.0 # initial % female
 pop_density = np.random.randint(50, 200) # number of people supported by a square kilometer of land (look for source)
 total_k = d**2 * pop_density
+max_start_pop = total_k/50
+min_start_pop = total_k/1000
 hostility = 3
 g = 0.5
